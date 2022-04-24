7043c959c295660336cbaa32c76c60c4c87593ed
Алексей <t1meal@yandex.ru>
2022-04-24 17:32:06 +0300

    Initial commit

---
+++ commons/src/main/java/ru/gb/storage/commons/Constant.java
@@ -0,0 +1,4 @@
+package ru.gb.storage.commons;
+
+public class Constant {
+}
---
+++ server/src/main/java/ru/gb/storage/server/Server.java
@@ -0,0 +1,7 @@
+package ru.gb.storage.server;
+
+public class Server {
+    public static void main(String[] args) {
+        System.out.println("Server started");
+    }
+}
