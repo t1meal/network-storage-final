01fdcab6c7b43f25d5bc63bf6d793a024a056124
Алексей <t1meal@yandex.ru>
2022-04-28 14:13:41 +0300

    сейчас пишу клиента. буду засовывать netty в отдельный поток для распаралеливания с GUI. с javaFX столько заморочек, и чтобы не терять время скорее всего буду переходить на взамодействие через командную строку. класс AuthorizationMessage еще не дописал.

--- client/src/main/java/ru/gb/storage/client/Client.java
+++ client/src/main/java/ru/gb/storage/client/Client.java
@@ -1,14 +1,48 @@
 package ru.gb.storage.client;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+import ru.gb.storage.client.controller.FxController;
+
+import java.io.File;
+import java.util.Objects;
 
 public class Client extends Application {
+    private final double WIDTH = 600;
+    private final double HEIGHT = 600;
+
     public static void main(String[] args) {
-        Application.launch(args);
+       launch(args);
+
     }
 
-    public void start(Stage stage) throws Exception {
-        stage.show();
+    public void start(Stage primaryStage) throws Exception {
+
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("sample.fxml"));
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("DropBox");
+        primaryStage.setWidth(WIDTH);
+        primaryStage.setHeight(HEIGHT);
+
+        primaryStage.show();
+
+
+
+
+//        stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+//            @Override
+//            public void handle(WindowEvent event) {
+//                System.out.println("Stage is closing");
+//                stage.close();
+//                System.exit(1);
+//
+//            }
+        }
     }
-}
+
+
+
